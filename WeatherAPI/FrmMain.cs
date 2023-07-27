93e65bb0fcf9414916d8666c690aea6e627e7e0f
Alex <Alex@PC-ALEX>
2023-07-27 17:59:41 +0800

    縣市補齊

--- WeatherAPI/FrmMain.cs
+++ WeatherAPI/FrmMain.cs
@@ -7,10 +7,9 @@ namespace WeatherAPI
 {
     public partial class FrmMain : Form
     {
-
         string FilePath = string.Empty;
+        List<string> City = new List<string>() { "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣", "臺北市", "新北市", "桃園市", "臺中市", "臺南市", "高雄市", "基隆市", "新竹縣", "新竹市", "苗栗縣", "彰化縣", "南投縣", "雲林縣", "嘉義縣", "嘉義市", "屏東縣" };
 
-        List<string> City = new List<string>() { "臺北市", "新北市", "基隆市", "新竹市" };
 
         public FrmMain()
         {
